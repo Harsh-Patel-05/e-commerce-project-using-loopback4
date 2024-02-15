cb11891a4fc0e8741e2042b9ce02b90d56739d1b
Harsh Patel <harsh53006@gmail.com>
2024-02-15 10:35:49 +0530

    commite

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,6 @@
-// export * from './user.controller';
-export * from './auth.controller';
 export * from './address.controller';
-export * from './product.controller';
+export * from './auth.controller';
 export * from './category.controller';
 export * from './product-variant.controller';
+export * from './product.controller';
+
