0b319fab7181dd021bce1f780e3d93161d93d2d6
Harsh Patel <harsh53006@gmail.com>
2024-02-21 10:37:34 +0530

    update files

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -1,10 +1,8 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
 
+
 @injectable({scope: BindingScope.TRANSIENT})
 export class PaymentService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(/* Add @inject to inject parameters */) { }
 
-  /*
-   * Add service methods here
-   */
 }
